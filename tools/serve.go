eb3b109d8f2dbf80d8338bd2d09e6c0939ea00b2
Jason Phan <jason@ket.so>
2022-01-24 17:35:30 -0600

    tools: Update serve and upload scripts for Zig

--- tools/serve.go
+++ tools/serve.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	port := "8000"
 	publicDir := "public"
-	fmt.Printf("Serving Go by Example at http://127.0.0.1:%s\n", port)
+	fmt.Printf("Serving Zig by Example at http://127.0.0.1:%s\n", port)
 	http.ListenAndServe(":"+port, http.FileServer(http.Dir(publicDir)))
 }
