45a122f5cba002a2c3d5f77d36bc72a1307e6a7a
Jason Phan <jason@ket.so>
2022-01-26 13:59:44 -0600

    examples: Add Integers

---
+++ examples/integers/integers.zig
@@ -0,0 +1,34 @@
+// Integers in Zig are just like in any other language... Right?
+
+const std = @import("std");
+
+pub fn main() anyerror!void {
+
+    // Well, it looks like the usual suspects are definitely here...
+    const a: u8 = 1;
+    const b: i32 = 10;
+    const c: u64 = 100;
+    const d: isize = 1_000;
+
+    // Hang on a second... Are those 42-bit and 69-bit integers? Yes they are! Zig
+    // supports arbitrary bit widths for integers.
+    const e: i42 = 10_000;
+    const f: u69 = 100_000;
+
+    // Zig also provides a type for compile-time known integers.
+    // `comptime_int`s have no size limit and are the default type of integer
+    // literals and Unicode code point literals.
+    const g: comptime_int = 1_000_000;
+    const h = 10_000_000;
+    const i = '💯';
+
+    std.debug.print("{}\n", .{a});
+    std.debug.print("{}\n", .{b});
+    std.debug.print("{}\n", .{c});
+    std.debug.print("{}\n", .{d});
+    std.debug.print("{}\n", .{e});
+    std.debug.print("{}\n", .{f});
+    std.debug.print("{}\n", .{g});
+    std.debug.print("{}\n", .{h});
+    std.debug.print("{}\n", .{i});
+}
