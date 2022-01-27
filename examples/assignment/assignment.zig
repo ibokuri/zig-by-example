22a3b432f13847f4832ad16f5e08e32f22367453
Jason Phan <jason@ket.so>
2022-01-27 12:28:19 -0600

    assignment: Clean up docs

--- examples/assignment/assignment.zig
+++ examples/assignment/assignment.zig
@@ -21,14 +21,13 @@ pub fn main() anyerror!void {
     std.debug.print("{}\n", .{inferred_c});
     std.debug.print("{}\n", .{inferred_v});
 
-    // The `undefined` value can be used to create uninitialized constants or
-    // variables. Note that type annotations are required here since the
-    // compiler has no type hints.
+    // If you want to create an uninitialized constant or variable, you must
+    // use the `undefined` value.
     const undefined_c: bool = undefined;
     var undefined_v: bool = undefined;
 
-    // Assignments can also be used to ignore expressions. For example, we use
-    // it here to reference, but not use, `undefined_c` and `undefined_v`.
+    // An assignment can also be used to ignore expressions. Here, we use it to
+    // reference our previous declarations.
     _ = undefined_c;
     _ = undefined_v;
 }
