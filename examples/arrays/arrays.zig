ce268128b112bc4a7c5109104e8d30083f9b243f
Jason Phan <jason@ket.so>
2022-01-27 19:48:11 -0600

    arrays: docs

--- examples/arrays/arrays.zig
+++ examples/arrays/arrays.zig
@@ -5,14 +5,14 @@ const std = @import("std");
 
 pub fn main() anyerror!void {
 
-    // To create a new array, you can write an _array literal_. Doing so
-    // allows the compiler to infer its size if it is `_`.
+    // To create an array, you can use an _array literal_. Doing so
+    // allows the compiler to infer its size if it is specified as `_`.
     const a = [3]i32{ 1, 2, 3 };
     const b = [_]i32{ 4, 5, 6 };
 
     // Another approach is to use an _anonymous list literal_. Here, the
-    // array's type is specified within a type annotation and the assigned
-    // anonymous list literal coerces to it.
+    // anonymous list literal on the right-hand side coerces to the array type
+    // specified in the type annotation.
     const c: [3]i32 = .{ 7, 8, 9 };
 
     // To see the size of an array, access its `len` field.
@@ -23,13 +23,13 @@ pub fn main() anyerror!void {
     std.debug.print("{any}\n", .{a ++ b});
     std.debug.print("{any}\n", .{[_]i32{0} ** 6});
 
-    // To access an element of an array, index into it using `[N]`.
+    // To access an element of an array, use the `[N]` syntax.
     var d: [3]i32 = undefined;
     d[0] = 10;
     d[1] = 11;
     d[2] = 12;
 
-    // To iterate over an array, a `for` loop can be used.
+    // To iterate over an array, you can use a `for` loop.
     for (d) |elem| {
         std.debug.print("{}\n", .{elem});
     }
