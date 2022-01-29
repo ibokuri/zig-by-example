fd22826e08116bc88b9f1833ee20e697b18a652c
Jason Phan <jason@ket.so>
2022-01-29 15:42:15 -0600

    pointers: docs

--- examples/pointers/pointers.zig
+++ examples/pointers/pointers.zig
@@ -1,26 +1,31 @@
-// A _pointer_ is an object that stores a memory address. Zig has two types of
-// pointers: single-item and many-item. Both pointer types are guaranteed to
-// point to a value.
+// A _pointer_ is an object that stores a memory address. Pointers in Zig come
+// in two forms: single- and many-item.
 
 const std = @import("std");
 
-// Single-item pointers point to a single memory address.
+// Single-item pointers point to exactly one value.
 const Single = *bool;
 
-// Many-item pointers point to an unknown number of memory addresses. They're
-// mostly used when interfacing with C code (e.g., C arrays), so we won't discuss
-// them here.
+// Many-item pointers point to an unknown number of values. Unless you're
+// interfacing with C code, you probably won't use these types of pointers, so
+// we'll skip over them.
 const Many = [*]bool;
+
+// Note that all pointers in Zig point to some non-`null` value. If you want
+// `null` pointers, you'll need to use <a
+// href="https://zig-by-example.com/optionals">Optionals</a>.
+const Null = ?*bool;
 
 pub fn main() anyerror!void {
 
-    // To create a single-item pointer from a value, use `&`.
+    // To create a single-item pointer, you use the `&` operator. Here, `ptr`
+    // is a pointer that stores `value`'s memory address.
     var value = true;
     var ptr: *bool = &value;
     std.debug.print("{}\n", .{ptr});
 
-    // To access the value pointed to by a single-item pointer, dereference the
-    // pointer using the `*` operator.
+    // To access the value located at the memory address stored by a
+    // single-item pointer, you use the `*` operator.
     std.debug.print("{}\n", .{ptr.*});
     ptr.* = false;
     std.debug.print("{}\n", .{ptr.*});
