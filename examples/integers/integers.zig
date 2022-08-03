22a2156f7fa6f8d8e2e746af2684bb28bf49c0ed
Jason Phan <jason@ket.so>
2022-08-02 22:11:31 -0500

    integers: Update

--- examples/integers/integers.zig
+++ examples/integers/integers.zig
@@ -1,19 +1,19 @@
-// _Integers_ in Zig are more or less as you'd expect... Sort of.
+// _Integers_ in Zig are pretty neat.
 
 const std = @import("std");
 
-// You certainly have your usual integer types...
+// You have your usual integer types...
 const a: u8 = 1;
 const b: i32 = 10;
 const c: i64 = 100;
 const d: usize = 1_000;
 
-// But then you also have arbitrary bit-width integers!
+// But you also have arbitrary bit-width integers.
 const e: u21 = 10_000;
 const f: i42 = 100_000;
 
-// Additionally, Zig supports compile-time known integers. These integers have
-// no size limit and can be written as integer literals or Unicode code point
+// Additionally, Zig supports compile-time known integers. These have no size
+// limit and can be written as either integer literals or Unicode code point
 // literals.
 const g: comptime_int = 1_000_000;
 const integer_literal = 10_000_000;
