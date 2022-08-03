b78fce778125fce400140eaafbca2600a743da8a
Jason Phan <jason@ket.so>
2022-08-03 18:13:44 -0500

    hello-world: Update

--- examples/hello-world/hello-world.sh
+++ examples/hello-world/hello-world.sh
@@ -1,8 +1,8 @@
-# To run a Zig program, use `zig run`.
+# To run this Zig program, use `zig run`.
 $ zig run hello-world.zig
 Hello, World!
 
-# To compile a Zig program, use `zig build-exe`.
+# To compile this Zig program, use `zig build-exe`.
 $ zig build-exe hello-world.zig
 $ ./hello-world
 Hello, World!
