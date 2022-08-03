09c4aa52e02c617da7d06f0964175c10179c21c9
Jason Phan <jason@ket.so>
2022-08-02 22:26:06 -0500

    arrays: Update

--- examples/arrays/arrays.zig
+++ examples/arrays/arrays.zig
@@ -1,29 +1,30 @@
-// In Zig, an _array_ is a fixed-size, sequential collection of elements of the
+// An _array_ is a fixed-size, sequential collection of elements of the
 // same type.
 
 const std = @import("std");
 
 pub fn main() anyerror!void {
 
-    // To create an array, you can use an _array literal_. Doing so
-    // allows the compiler to infer its size if it is specified as `_`.
+    // To make an array, you can use an _array literal_. The compiler
+    // will infer the array's size if it is specified as `_`.
     const a = [3]i32{ 1, 2, 3 };
     const b = [_]i32{ 4, 5, 6 };
 
-    // Another approach is to use an _anonymous list literal_. Here, the
-    // anonymous list literal on the right-hand side coerces to the array type
-    // specified in the type annotation.
+    // You can also use an _anonymous list literal_. Here, the value on the
+    // right-hand side coerces to the array type specified on the left-hand
+    // side.
     const c: [3]i32 = .{ 7, 8, 9 };
 
-    // To see the size of an array, access its `len` field.
+    // Every array has a `len` field that stores the array's size.
     std.debug.print("{}\n", .{c.len});
 
-    // If two arrays are compile-time known, you can concatenate and repeat
-    // them.
-    std.debug.print("{any}\n", .{a ++ b});
+    // If an array is compile-time known, it can be repeated.
     std.debug.print("{any}\n", .{[_]i32{0} ** 6});
 
-    // To access an element of an array, use the `[N]` syntax.
+    // If two arrays are compile-time known, they can be concatenated.
+    std.debug.print("{any}\n", .{a ++ b});
+
+    // To access an element of an array, use the `[N]` syntax, where `N` is the position of the element (starting from 0).
     var d: [3]i32 = undefined;
     d[0] = 10;
     d[1] = 11;
