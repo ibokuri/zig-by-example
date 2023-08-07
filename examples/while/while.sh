0d161b2c031814faa13d8543882fb636372bbd09
Danh Phan <input@danh.computer>
2023-08-07 09:51:23 -0500

    while: Update

--- examples/while/while.sh
+++ examples/while/while.sh
@@ -1,7 +1,9 @@
 $ zig run while.zig
-i: 0
-i: 1
-j: 0
-j: 1
-k: 0
-k: 1
+a: 0
+a: 1
+b: 0
+b: 1
+c: 0
+c: 2
+d: 0
+d: 1
--- examples/while/while.zig
+++ examples/while/while.zig
@@ -4,21 +4,30 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    var i: usize = 0;
-    var j: usize = 0;
-    var k: usize = 0;
+    var a: usize = 0;
+    var b: usize = 0;
+    var c: usize = 0;
+    var d: usize = 0;
 
     // The body of a while loop will repeatedly execute until the loop's
     // condition is no longer true.
-    while (i < 2) {
-        print("i: {}\n", .{i});
-        i += 1;
+    while (a < 2) {
+        print("a: {}\n", .{a});
+        a += 1;
     }
 
-    // _Continue expressions_ can be specified to evaluate an expression after
-    // each execution of a while loop's body.
-    while (j < 2) : (j += 1) {
-        print("j: {}\n", .{j});
+    // _Continue expressions_ can be used to evaluate an expression after each
+    // execution of a while loop's body.
+    while (b < 2) : (b += 1) {
+        print("b: {}\n", .{b});
+    }
+
+    // You can specify multiple continue expressions by using the `({ <expr1>; <expr2>; ... })` syntax.
+    while (c < 4) : ({
+        c += 1;
+        c += 1;
+    }) {
+        print("c: {}\n", .{c});
     }
 
     // To break out of a while loop, use the `break` keyword.
@@ -28,9 +37,9 @@ pub fn main() !void {
 
     // To jump to the next iteration of a while loop, use the `continue`
     // keyword.
-    while (true) : (k += 1) {
-        if (k < 2) {
-            print("k: {}\n", .{k});
+    while (true) : (d += 1) {
+        if (d < 2) {
+            print("d: {}\n", .{d});
             continue;
         }
 
