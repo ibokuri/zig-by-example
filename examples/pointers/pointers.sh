9ddf2064b73d31575f93923d487e83e315e4fd4d
Danh Phan <input@danh.computer>
2023-08-06 22:39:41 -0500

    pointers: Update

--- examples/pointers/pointers.sh
+++ examples/pointers/pointers.sh
@@ -1,5 +1,3 @@
 $ zig run pointers.zig
 pointer: bool@7ff7b26e24a7
-before: true
-after: false
-pointer: bool@10d869c68
+value: true
--- examples/pointers/pointers.zig
+++ examples/pointers/pointers.zig
@@ -1,38 +1,41 @@
-// A _pointer_ is an object that stores a memory address. There are three types
-// of pointers in Zig: single-item, many-item, and <a
-// href="https://zig-by-example.com/for">Slices</a>.
+// _Pointers_ are objects that store a memory address.
 
 const std = @import("std");
 const print = std.debug.print;
 
-// Single-item pointers point to exactly one value.
+// _Single-item pointers_ point to exactly one value.
 const Single = *bool;
 
-// Many-item pointers point to an unknown number of values. Unless you're
+// _Many-item pointers_ point to an unknown number of values. Unless you're
 // interfacing with C code, you probably won't use these types of pointers, so
 // we'll skip over them.
 const Many = [*]bool;
 
-// Note that all pointers in Zig point to a non-`null` value. To get
-// `null` pointers, you need to use <a
-// href="https://zig-by-example.com/optionals">Optionals</a>.
+// All pointers in Zig point to a non-`null` value. To define a `null` pointer,
+// you must make the pointer type
+// <a href="https://zig-by-example.com/optionals">optional</a>.
 const Null = ?*bool;
 
 pub fn main() anyerror!void {
 
     // To create a single-item pointer, use the `&` operator.
-    var value = true;
+    var value = false;
     var ptr: *bool = &value;
     print("pointer: {}\n", .{ptr});
 
     // To access the value located at the memory address stored by a
     // single-item pointer, use the `*` operator.
-    print("before: {}\n", .{ptr.*});
-    ptr.* = false;
-    print("after: {}\n", .{ptr.*});
+    ptr.* = true;
+    print("value: {}\n", .{ptr.*});
 
-    // Note that if a value is `const`, its pointer type is also `const`.
+    // If a pointer is `const`, its pointee cannot be re-assigned.
+    const const_ptr: *bool = &value;
+
+    // If a pointee is `const`, its pointer type will reflect that. Naturally,
+    // the value of `const` pointees cannot be modified.
     const const_value = true;
     var ptr_to_const: *const bool = &const_value;
-    print("pointer: {}\n", .{ptr_to_const});
+
+    _ = ptr_to_const;
+    _ = const_ptr;
 }
