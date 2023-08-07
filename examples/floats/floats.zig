3b0b3e523c762bb4766c3be1ac0be5affc6be0b7
Danh Phan <input@danh.computer>
2023-08-06 22:04:28 -0500

    floats: Update

--- examples/floats/floats.zig
+++ examples/floats/floats.zig
@@ -4,13 +4,13 @@
 const std = @import("std");
 const print = std.debug.print;
 
-// Here, we define some common floating-point types.
+// Here, we define some common floating types.
 const a: f16 = 1.0;
 const b: f32 = 100.0;
 const c: f64 = 1_000.0;
 const d: f128 = 10_000.0;
 
-// Here, we define some _compile-time_ known floating-points. These have
+// Here, we define some _compile-time_ known floats. These have
 // no size limit and are written as float literals.
 const e: comptime_float = 100_000.0;
 const f = 1_000_000.0;
