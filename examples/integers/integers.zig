43c3e22cc9414e378ff70a7f8f016d450445dd65
Danh Phan <input@danh.computer>
2023-08-06 22:01:07 -0500

    integers: Update

--- examples/integers/integers.zig
+++ examples/integers/integers.zig
@@ -1,4 +1,4 @@
-// _Integers_ in Zig have arbitrary bit-widths.
+// _Integers_ in Zig can have arbitrary bit-widths.
 
 const std = @import("std");
 const print = std.debug.print;
@@ -13,7 +13,7 @@ const d: isize = 1_000;
 const e: u21 = 10_000;
 const f: i42 = 100_000;
 
-// Here, we define some compile-time known integers. These have no size limit
+// Here, we define some _compile-time_ known integers. These have no size limit
 // and can be written as either integer literals or Unicode code point
 // literals.
 const g: comptime_int = 1_000_000;
@@ -21,6 +21,6 @@ const h = 10_000_000;
 const i = '💯';
 
 pub fn main() anyerror!void {
-    print("integer: {}\n", .{i});
+    print("integer: {d}\n", .{i});
     print("unicode: {u}\n", .{i});
 }
