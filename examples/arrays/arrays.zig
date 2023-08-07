23b63010e360c189c4fd5db942eb448a7e525160
Danh Phan <input@danh.computer>
2023-08-06 22:16:08 -0500

    arrays: Update

--- examples/arrays/arrays.zig
+++ examples/arrays/arrays.zig
@@ -1,20 +1,29 @@
-// An _array_ is a fixed-size, sequential collection of elements of the
-// same type.
+// _Arrays_ are fixed-size, sequential collections of elements of the same
+// type.
 
 const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() anyerror!void {
 
-    // To make an array, you can use an _array literal_. The compiler
-    // will infer the array's size if it is specified as `_`.
+    // Here, we define an array using the _array literal_ syntax.
     const a = [3]i32{ 1, 2, 3 };
+
+    // Here, we define an array with a size of `_`. This tells the compiler
+    // to infer the array's size for us (in this case, `3`).
     const b = [_]i32{ 4, 5, 6 };
 
-    // You can also use an _anonymous list literal_. Here, the value on the
-    // right-hand side coerces to the array type specified on the left-hand
-    // side.
+    // Here, we define an array using an _anonymous list literal_. The value on
+    // the right-hand side will coerce to the array type specified on the
+    // left-hand side.
     const c: [3]i32 = .{ 7, 8, 9 };
+
+    // To access the elements of an array, use the `[N]` syntax, where `N` is
+    // the position of the element (starting from 0).
+    var d: [3]i32 = undefined;
+    d[0] = 10;
+    d[1] = 11;
+    d[2] = 12;
 
     // Every array has a `len` field that stores the array's size.
     print("len: {}\n", .{c.len});
@@ -24,13 +33,6 @@ pub fn main() anyerror!void {
 
     // If two arrays are compile-time known, they can be concatenated.
     print("concat: {any}\n", .{a ++ b});
-
-    // To access an element of an array, use the `[N]` syntax, where `N` is the
-    // position of the element (starting from 0).
-    var d: [3]i32 = undefined;
-    d[0] = 10;
-    d[1] = 11;
-    d[2] = 12;
 
     // To iterate over an array, you can use a <a
     // href="https://zig-by-example.com/for">For loop</a>.
