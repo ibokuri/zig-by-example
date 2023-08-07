192e73ec3baf93b50ee361913d893f5bb69d7bbb
Danh Phan <input@danh.computer>
2023-08-06 22:03:58 -0500

    floats: Update

--- examples/floats/floats.zig
+++ examples/floats/floats.zig
@@ -1,19 +1,20 @@
-// _Floating-points_ in Zig are also pretty neat (less so than integers, though).
+// _Floating-points_ in Zig are similar to integers, though they cannot have
+// arbitrary bit-widths.
 
 const std = @import("std");
 const print = std.debug.print;
 
-// You have your usual floating-point types...
+// Here, we define some common floating-point types.
 const a: f16 = 1.0;
 const b: f32 = 100.0;
 const c: f64 = 1_000.0;
 const d: f128 = 10_000.0;
 
-// And a type for compile-time known floating-point values. These have no size
-// limit and are written as float literals.
+// Here, we define some _compile-time_ known floating-points. These have
+// no size limit and are written as float literals.
 const e: comptime_float = 100_000.0;
-const float_literal = 1_000_000.0;
+const f = 1_000_000.0;
 
 pub fn main() anyerror!void {
-    print("float: {}\n", .{float_literal});
+    print("float: {}\n", .{f});
 }
